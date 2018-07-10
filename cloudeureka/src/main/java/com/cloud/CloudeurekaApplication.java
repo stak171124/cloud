a70a327ac8c816c4d4ddb3919fc608bcf40bed47
wang <565729063@qq.com>
2018-07-10 22:20:50 +0800

    1

---
+++ cloudeureka/src/main/java/com/cloud/CloudeurekaApplication.java
@@ -0,0 +1,12 @@
+package com.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudeurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudeurekaApplication.class, args);
+	}
+}
